de4cc0028792640fd4f75fee61d635177a30d4b6
Trevor Summerfield <tresummerfield@gmail.com>
2015-11-08 00:07:32 -0500

    fixed

--- receive.js
+++ receive.js
@@ -40,7 +40,7 @@ app.post('/receive', function(request, respond) {
 
 app.get('/cpu_usage', function(request, response) {
 
-    DataEntry.find().sort({timestamp: -1}).limit(10).exec(function(err, entries) {
+    DataEntry.find().sort({timestamp: -1}).exec(function(err, entries) {
         if (err) {
             console.log(err);
         }
