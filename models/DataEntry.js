65d41dd9f3742bd99eb47790fa03eaf5d28cc0d4
Trevor Summerfield <tresummerfield@gmail.com>
2015-11-08 10:58:07 -0500

    new architecture

--- models/DataEntry.js
+++ models/DataEntry.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DataEntry = new Schema({
-    timestamp: {type: Date, required: true, index: true},
-    cpu_usage: {type: Number, required: true},
-    mem_free: {type: Number, required: true},
-    mem_cache: {type: Number, required: true},
-    mem_used: {type: Number, required: true},
+    timestamp: {type: Date, index: true},
+    cpu_usage: {type: Number},
+    mem_free: {type: Number,
+    mem_cache: {type: Number},
+    mem_used: {type: Number},
     cpu0: {type: Number},
     cpu1: {type: Number},
     cpu2: {type: Number},
--- receive.js
+++ receive.js
@@ -5,8 +5,7 @@ var mongoose = require('mongoose')
 var app = express();
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
-
-var cors = require('cors')
+var cors = require('cors');
 var count = 0;
 
 mongoose.connect('mongodb://localhost/myapp');
@@ -16,46 +15,13 @@ var DataEntry = require('./models/DataEntry');
 app.use(cors())
 app.use(bodyParser.json());
 
-app.post('/receive', function(request, respond) {
-
-    var timestamp = request.body.payload[0].timestamp;
-    var load = request.body.payload[0].LONGTERM.Load;
-    var mem_free = request.body.payload[0].LONGTERM['Memory.real.free'];
-    var mem_cache = request.body.payload[0].LONGTERM['Memory.real.cache'];
-    var mem_used = request.body.payload[0].LONGTERM['Memory.real.used'];
-    // var cpu0 = request.body.payload[0].LONGTERM['CPU.cpu0.user'];
-    // var cpu1 = request.body.payload[0].LONGTERM['CPU.cpu1.user'];
-    // var cpu2 = request.body.payload[0].LONGTERM['CPU.cpu2.user'];
-    // var cpu3 = request.body.payload[0].LONGTERM['CPU.cpu3.user'];
-
-    exec('./load.sh', function(error, stdout, sterr) {
-        var num = stdout.split(' ');
-
-        var d = new Date(0);
-        d.setUTCSeconds(timestamp);
-
-        var de = new DataEntry({
-            timestamp: d,
-            cpu_usage: load,
-            mem_free: mem_free,
-            mem_used: mem_used,
-            mem_cache: mem_cache,
-            cpu0: num[0],
-            cpu1: num[1],
-            cpu2: num[2],
-            cpu3: num[3]
-        });
-
-        de.save(function(err) {
-            if (err) {
-                console.log(err);
-            }
-            respond.status(200).send();
-        });
-    });
-
-
-
+app.get('/clear', function(request, response) {
+    DataEntry.remove({}. function(err) {
+        if (err) {
+            console.log(err);
+        }
+        response.send();
+    })
 });
 
 app.get('/stats', function(request, response) {
@@ -79,5 +45,31 @@ app.get('/latest', function(request, response) {
         response.send(entries);
     });
 });
+
+setInterval(function() {
+
+    exec('./load.sh', function(error, stdout, sterr) {
+        var num = stdout.split(' ');
+
+        var de = new DataEntry({
+            timestamp: Date.now(),
+            // cpu_usage: load,
+            // mem_free: mem_free,
+            // mem_used: mem_used,
+            // mem_cache: mem_cache,
+            cpu0: num[0],
+            cpu1: num[1],
+            cpu2: num[2],
+            cpu3: num[3]
+        });
+
+        de.save(function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+
+}, 5000);
 
 app.listen(9235);
