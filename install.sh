d3c0a242f52e98581d6edf52a4c33e5dbc33d250
Trevor Summerfield <tresummerfield@gmail.com>
2015-11-08 12:54:05 -0500

    should probably apt-get update first

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install ShortView Daemon on Ubuntu Server
+apt-get update
 apt-get install curl nodejs-legacy npm git stress mongodb -y
 git clone https://github.com/ShortView/ShortViewDaemon /opt/shortview
 cd /opt/shortview
