ebcc8f14244d2dba550f5c2d8b96665cd2449d33
Trevor Summerfield <tresummerfield@gmail.com>
2015-11-08 12:49:22 -0500

    auto-install script for ubuntu server

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-get install curl nodejs-legacy npm git stress mongodb -y
+git clone https://github.com/ShortView/ShortViewDaemon /opt/shortview
+cd /opt/shortview
+npm install
+cp shortview.service /etc/systemd/system/
+systemctl enable shortview.service
+systemctl start shortview.service
+echo "--------------------------------------"
+echo "ShortView Dameon installed and running"
+ip=$(curl -s http://api.ipify.org/)
+echo "Connect at: $ip:9235"
